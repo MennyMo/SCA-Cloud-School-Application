a0b124b40a12535f8e86df9a2dae870fe830bc83
MennyMo <molade779@gmail.com>
2022-04-03 18:12:54 +0100

    adding test file to feature

--- docker/app.js
+++ docker/app.js
@@ -7,9 +7,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/public/index.html`);
 });
-
-app.get("/", (req, res) => res.send("Welcome to SCA Cloud School Application"));
-
 	app.listen(5004, () => {
-	console.log("This app is listening on port 5000!");
+	console.log("running...");
 });
